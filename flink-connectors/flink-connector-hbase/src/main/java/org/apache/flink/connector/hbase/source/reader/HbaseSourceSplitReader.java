216306ed079f7cfe2eacfaf17dedebdfc8491adf
Moritz Manner <moritz.manner@gmail.com>
2021-01-03 21:42:28 +0100

    Test hbase source

--- flink-connectors/flink-connector-hbase/src/main/java/org/apache/flink/connector/hbase/source/reader/HbaseSourceSplitReader.java
+++ flink-connectors/flink-connector-hbase/src/main/java/org/apache/flink/connector/hbase/source/reader/HbaseSourceSplitReader.java
@@ -6,15 +6,28 @@ import org.apache.flink.connector.base.source.reader.splitreader.SplitReader;
 import org.apache.flink.connector.base.source.reader.splitreader.SplitsChange;
 import org.apache.flink.connector.hbase.source.split.HbaseSourceSplit;
 
+import javax.annotation.Nullable;
+
 import java.io.IOException;
+import java.util.Arrays;
+import java.util.Collection;
+import java.util.List;
+import java.util.Set;
 
 /**
  * A {@link SplitReader} implementation for Hbase.
  */
 public class HbaseSourceSplitReader<T> implements SplitReader<Tuple3<T, Long, Long>, HbaseSourceSplit> {
+	private List<Tuple3<T, Long, Long>> records;
+
+	public HbaseSourceSplitReader() {
+		records = Arrays.asList(new Tuple3((T) "a", 1, 1), new Tuple3((T) "a", 1, 1));
+	}
+
 	@Override
 	public RecordsWithSplitIds<Tuple3<T, Long, Long>> fetch() throws IOException {
-		return null;
+		HbaseSplitRecords<Tuple3<T, Long, Long>> records = new HbaseSplitRecords<>(this.records);
+		return records;
 	}
 
 	@Override
@@ -31,4 +44,34 @@ public class HbaseSourceSplitReader<T> implements SplitReader<Tuple3<T, Long, Lo
 	public void close() throws Exception {
 
 	}
+
+	private static class HbaseSplitRecords<T> implements RecordsWithSplitIds<T> {
+		private final Collection<T> records;
+
+		private HbaseSplitRecords(Collection<T> records) {
+			this.records = records;
+		}
+
+		@Nullable
+		@Override
+		public String nextSplit() {
+			return null;
+		}
+
+		@Nullable
+		@Override
+		public T nextRecordFromSplit() {
+			if (records.iterator().hasNext()) {
+				return null;
+			} else {
+				return records.iterator().next();
+			}
+		}
+
+		@Override
+		public Set<String> finishedSplits() {
+			return null;
+		}
+	}
+
 }
