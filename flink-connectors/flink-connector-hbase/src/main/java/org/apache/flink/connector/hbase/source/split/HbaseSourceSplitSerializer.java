986b421f96f221f81ce40e4f5a533d5618727daa
Moritz Manner <moritz.manner@gmail.com>
2021-01-03 21:55:22 +0100

    Add split serializer

---
+++ flink-connectors/flink-connector-hbase/src/main/java/org/apache/flink/connector/hbase/source/split/HbaseSourceSplitSerializer.java
@@ -0,0 +1,32 @@
+package org.apache.flink.connector.hbase.source.split;
+
+import org.apache.flink.core.io.SimpleVersionedSerializer;
+
+import java.io.ByteArrayOutputStream;
+import java.io.DataOutputStream;
+import java.io.IOException;
+
+/**
+ * The {@link org.apache.flink.core.io.SimpleVersionedSerializer serializer} for {@link HbaseSourceSplit}.
+ */
+public class HbaseSourceSplitSerializer implements SimpleVersionedSerializer<HbaseSourceSplit> {
+	@Override
+	public int getVersion() {
+		return 0;
+	}
+
+	@Override
+	public byte[] serialize(HbaseSourceSplit obj) throws IOException {
+		try (ByteArrayOutputStream baos = new ByteArrayOutputStream();
+				DataOutputStream out = new DataOutputStream(baos)) {
+			out.writeUTF("test");
+			out.flush();
+			return baos.toByteArray();
+		}
+	}
+
+	@Override
+	public HbaseSourceSplit deserialize(int version, byte[] serialized) throws IOException {
+		return new HbaseSourceSplit();
+	}
+}
