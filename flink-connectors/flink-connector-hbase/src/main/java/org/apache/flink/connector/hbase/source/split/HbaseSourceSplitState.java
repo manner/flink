72b02d987f02ccc4ac3b48e980086432423c5c01
Moritz Manner <moritz.manner@gmail.com>
2021-01-04 23:40:32 +0100

    Add prototype code add adhere to checkstyle

--- flink-connectors/flink-connector-hbase/src/main/java/org/apache/flink/connector/hbase/source/split/HbaseSourceSplitState.java
+++ flink-connectors/flink-connector-hbase/src/main/java/org/apache/flink/connector/hbase/source/split/HbaseSourceSplitState.java
@@ -4,7 +4,7 @@ package org.apache.flink.connector.hbase.source.split;
  * Blah.
  */
 public class HbaseSourceSplitState extends HbaseSourceSplit {
-	public HbaseSourceSplitState(String s) {
-		super(s);
+	public HbaseSourceSplitState(String id) {
+		super(id);
 	}
 }
