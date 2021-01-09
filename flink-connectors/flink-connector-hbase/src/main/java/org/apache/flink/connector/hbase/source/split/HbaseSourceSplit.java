1266915ba042fd920563572c87a44f05f909fd5b
Moritz Manner <moritz.manner@gmail.com>
2021-01-09 23:09:58 +0100

    Add host, table and hbase conf to HbaseSourceSplit

--- flink-connectors/flink-connector-hbase/src/main/java/org/apache/flink/connector/hbase/source/split/HbaseSourceSplit.java
+++ flink-connectors/flink-connector-hbase/src/main/java/org/apache/flink/connector/hbase/source/split/HbaseSourceSplit.java
@@ -2,14 +2,42 @@ package org.apache.flink.connector.hbase.source.split;
 
 import org.apache.flink.api.connector.source.SourceSplit;
 
+import org.apache.hadoop.conf.Configuration;
+
+import java.io.Serializable;
+
 /**
  * A {@link SourceSplit} for a Hbase.
  */
-public class HbaseSourceSplit implements SourceSplit {
+public class HbaseSourceSplit implements SourceSplit, Serializable {
+
+	private static final long serialVersionUID = 1L;
+
 	private final String id;
 
-	public HbaseSourceSplit(String id) {
+	private final String table;
+
+	private final String host;
+
+	private final Configuration hbaseConf;
+
+	public HbaseSourceSplit(String id, String host, String table, Configuration hbaseConf) {
 		this.id = id;
+		this.host = host;
+		this.table = table;
+		this.hbaseConf = hbaseConf;
+	}
+
+	public String getTable() {
+		return table;
+	}
+
+	public String getHost() {
+		return host;
+	}
+
+	public Configuration getHbaseConf() {
+		return hbaseConf;
 	}
 
 	@Override
