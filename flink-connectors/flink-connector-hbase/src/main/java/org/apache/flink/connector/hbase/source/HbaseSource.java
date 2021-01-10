c439ae789d5a9b63396d6a9f2b2eb6bbc134576c
Moritz Manner <moritz.manner@gmail.com>
2021-01-10 01:17:13 +0100

    Simplify Source

--- flink-connectors/flink-connector-hbase/src/main/java/org/apache/flink/connector/hbase/source/HbaseSource.java
+++ flink-connectors/flink-connector-hbase/src/main/java/org/apache/flink/connector/hbase/source/HbaseSource.java
@@ -7,17 +7,20 @@ import org.apache.flink.api.connector.source.SourceReaderContext;
 import org.apache.flink.api.connector.source.SplitEnumerator;
 import org.apache.flink.api.connector.source.SplitEnumeratorContext;
 import org.apache.flink.configuration.Configuration;
-import org.apache.flink.connector.hbase.source.enumerator.HbaseSourceEnumState;
 import org.apache.flink.connector.hbase.source.enumerator.HbaseSourceEnumerator;
 import org.apache.flink.connector.hbase.source.reader.HbaseSourceReader;
 import org.apache.flink.connector.hbase.source.split.HbaseSourceSplit;
 import org.apache.flink.connector.hbase.source.split.HbaseSourceSplitSerializer;
 import org.apache.flink.core.io.SimpleVersionedSerializer;
 
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.List;
+
 /**
  * A connector for Hbase.
  */
-public class HbaseSource implements Source<byte[], HbaseSourceSplit, HbaseSourceEnumState> {
+public class HbaseSource implements Source<byte[], HbaseSourceSplit, Collection<HbaseSourceSplit>> {
 
 	private final Boundedness boundedness;
 
@@ -37,19 +40,25 @@ public class HbaseSource implements Source<byte[], HbaseSourceSplit, HbaseSource
 	}
 
 	@Override
-	public SplitEnumerator<HbaseSourceSplit, HbaseSourceEnumState> restoreEnumerator(
+	public SplitEnumerator<HbaseSourceSplit, Collection<HbaseSourceSplit>> restoreEnumerator(
 		SplitEnumeratorContext<HbaseSourceSplit> enumContext,
-		HbaseSourceEnumState checkpoint) throws Exception {
+		Collection<HbaseSourceSplit> checkpoint) throws Exception {
 		System.out.println("restoreEnumerator");
 
 		return null;
 	}
 
 	@Override
-	public SplitEnumerator<HbaseSourceSplit, HbaseSourceEnumState> createEnumerator(
+	public SplitEnumerator<HbaseSourceSplit, Collection<HbaseSourceSplit>> createEnumerator(
 		SplitEnumeratorContext<HbaseSourceSplit> enumContext) throws Exception {
 		System.out.println("createEnumerator");
-		return new HbaseSourceEnumerator(enumContext);
+		List<HbaseSourceSplit> splits = new ArrayList<>();
+		splits.add(new HbaseSourceSplit(
+			"1234",
+			"localhost",
+			"test",
+			new org.apache.hadoop.conf.Configuration()));
+		return new HbaseSourceEnumerator(enumContext, splits);
 	}
 
 	@Override
@@ -60,7 +69,7 @@ public class HbaseSource implements Source<byte[], HbaseSourceSplit, HbaseSource
 	}
 
 	@Override
-	public SimpleVersionedSerializer<HbaseSourceEnumState> getEnumeratorCheckpointSerializer() {
+	public SimpleVersionedSerializer<Collection<HbaseSourceSplit>> getEnumeratorCheckpointSerializer() {
 		System.out.println("getEnumeratorCheckpointSerializer");
 
 		return null;
