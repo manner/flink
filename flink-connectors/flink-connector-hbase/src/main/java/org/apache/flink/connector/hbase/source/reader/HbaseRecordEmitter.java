4c65bd13feff499aafa02bd1142e2229cf976c39
Moritz Manner <moritz.manner@gmail.com>
2021-01-10 16:45:43 +0100

    HbaseSource outputs hardcoded value

--- flink-connectors/flink-connector-hbase/src/main/java/org/apache/flink/connector/hbase/source/reader/HbaseRecordEmitter.java
+++ flink-connectors/flink-connector-hbase/src/main/java/org/apache/flink/connector/hbase/source/reader/HbaseRecordEmitter.java
@@ -4,6 +4,8 @@ import org.apache.flink.api.connector.source.SourceOutput;
 import org.apache.flink.connector.base.source.reader.RecordEmitter;
 import org.apache.flink.connector.hbase.source.split.HbaseSourceSplitState;
 
+import java.util.Arrays;
+
 
 /**
  * The {@link RecordEmitter} implementation for {@link HbaseSourceReader}.
@@ -15,7 +17,7 @@ public class HbaseRecordEmitter implements RecordEmitter<byte[], byte[], HbaseSo
 		byte[] element,
 		SourceOutput<byte[]> output,
 		HbaseSourceSplitState splitState) {
-		System.out.println("emitRecord");
+		System.out.println("emitRecord: " + Arrays.toString(element));
 		output.collect(element);
 	}
 }
