87b0a655bbb881ab2ef7b062c4cee8ef890d503d
Moritz Manner <moritz.manner@gmail.com>
2021-01-10 01:17:13 +0100

    Simplify Source

--- flink-connectors/flink-connector-hbase/src/main/java/org/apache/flink/connector/hbase/source/reader/HbaseRecordEmitter.java
+++ flink-connectors/flink-connector-hbase/src/main/java/org/apache/flink/connector/hbase/source/reader/HbaseRecordEmitter.java
@@ -1,7 +1,6 @@
 package org.apache.flink.connector.hbase.source.reader;
 
 import org.apache.flink.api.connector.source.SourceOutput;
-import org.apache.flink.api.java.tuple.Tuple3;
 import org.apache.flink.connector.base.source.reader.RecordEmitter;
 import org.apache.flink.connector.hbase.source.split.HbaseSourceSplitState;
 
@@ -9,14 +8,14 @@ import org.apache.flink.connector.hbase.source.split.HbaseSourceSplitState;
 /**
  * The {@link RecordEmitter} implementation for {@link HbaseSourceReader}.
  */
-public class HbaseRecordEmitter implements RecordEmitter<Tuple3<byte[], Long, Long>, byte[], HbaseSourceSplitState> {
+public class HbaseRecordEmitter implements RecordEmitter<byte[], byte[], HbaseSourceSplitState> {
 
 	@Override
 	public void emitRecord(
-		Tuple3<byte[], Long, Long> element,
+		byte[] element,
 		SourceOutput<byte[]> output,
 		HbaseSourceSplitState splitState) {
 		System.out.println("emitRecord");
-		output.collect(element.f0, element.f2);
+		output.collect(element);
 	}
 }
