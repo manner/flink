1bab5736bdfaa040760447fc177760f36150c949
Leon Bein <leon.bein@student.hpi.de>
2021-01-13 17:01:45 +0100

    Cleanup unused classes and rename SourceEnumerator to SplitEnumerator

---
+++ flink-connectors/flink-connector-hbase/src/main/java/org/apache/flink/connector/hbase/source/enumerator/HbaseSplitEnumerator.java
@@ -0,0 +1,66 @@
+package org.apache.flink.connector.hbase.source.enumerator;
+
+import org.apache.flink.annotation.Internal;
+import org.apache.flink.api.connector.source.SplitEnumerator;
+import org.apache.flink.api.connector.source.SplitEnumeratorContext;
+import org.apache.flink.connector.hbase.source.split.HbaseSourceSplit;
+
+import javax.annotation.Nullable;
+
+import java.util.ArrayDeque;
+import java.util.Collection;
+import java.util.List;
+import java.util.Queue;
+
+/** The enumerator class for Hbase source. */
+@Internal
+public class HbaseSplitEnumerator
+        implements SplitEnumerator<HbaseSourceSplit, Collection<HbaseSourceSplit>> {
+    private final SplitEnumeratorContext<HbaseSourceSplit> context;
+    private Queue<HbaseSourceSplit> remainingSplits;
+
+    public HbaseSplitEnumerator(
+            SplitEnumeratorContext<HbaseSourceSplit> context, Collection<HbaseSourceSplit> splits) {
+        this.context = context;
+        this.remainingSplits = new ArrayDeque<>(splits);
+    }
+
+    @Override
+    public void start() {
+        System.out.println("Starting HbaseSourceEnumerator");
+    }
+
+    @Override
+    public void close() {}
+
+    @Override
+    public void handleSplitRequest(int subtaskId, @Nullable String requesterHostname) {
+        final HbaseSourceSplit nextSplit = remainingSplits.poll();
+        if (nextSplit != null) {
+            context.assignSplit(nextSplit, subtaskId);
+        } else {
+            context.signalNoMoreSplits(subtaskId);
+        }
+    }
+
+    @Override
+    public void addSplitsBack(List<HbaseSourceSplit> splits, int subtaskId) {
+        remainingSplits.addAll(splits);
+    }
+
+    @Override
+    public void addReader(int subtaskId) {
+        System.out.println("addReader");
+        HbaseSourceSplit nextSplit = remainingSplits.poll();
+        if (nextSplit != null) {
+            context.assignSplit(nextSplit, subtaskId);
+        } else {
+            context.signalNoMoreSplits(subtaskId);
+        }
+    }
+
+    @Override
+    public Collection<HbaseSourceSplit> snapshotState() throws Exception {
+        return remainingSplits;
+    }
+}
