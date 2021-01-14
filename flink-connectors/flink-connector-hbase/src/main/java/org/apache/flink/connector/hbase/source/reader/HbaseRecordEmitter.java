3cfb3cebf65368dbc55a6ca3148239a22379302f
Leon Bein <leon.bein@student.hpi.de>
2021-01-14 16:48:36 +0100

    Reformat prototype classes according to flink code automformat

--- flink-connectors/flink-connector-hbase/src/main/java/org/apache/flink/connector/hbase/source/reader/HbaseRecordEmitter.java
+++ flink-connectors/flink-connector-hbase/src/main/java/org/apache/flink/connector/hbase/source/reader/HbaseRecordEmitter.java
@@ -6,18 +6,13 @@ import org.apache.flink.connector.hbase.source.split.HbaseSourceSplitState;
 
 import java.util.Arrays;
 
-
-/**
- * The {@link RecordEmitter} implementation for {@link HbaseSourceReader}.
- */
+/** The {@link RecordEmitter} implementation for {@link HbaseSourceReader}. */
 public class HbaseRecordEmitter implements RecordEmitter<byte[], byte[], HbaseSourceSplitState> {
 
-	@Override
-	public void emitRecord(
-		byte[] element,
-		SourceOutput<byte[]> output,
-		HbaseSourceSplitState splitState) {
-		System.out.println("emitRecord: " + Arrays.toString(element));
-		output.collect(element);
-	}
+    @Override
+    public void emitRecord(
+            byte[] element, SourceOutput<byte[]> output, HbaseSourceSplitState splitState) {
+        System.out.println("emitRecord: " + Arrays.toString(element));
+        output.collect(element);
+    }
 }
