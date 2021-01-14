71ddcbd6a1a380ab76b5c54344242dee461a0aee
Leon Bein <leon.bein@student.hpi.de>
2021-01-14 19:18:12 +0100

    [Intermediate] Get hbase test cluster starter running
    - Moved to test to get access to test depndencies (lead to changing how hbase configs were passed)
    - Added old versions of hadoop dependencies

--- flink-connectors/flink-connector-hbase/src/main/java/org/apache/flink/connector/hbase/source/HbaseSource.java
+++ flink-connectors/flink-connector-hbase/src/main/java/org/apache/flink/connector/hbase/source/HbaseSource.java
@@ -7,23 +7,37 @@ import org.apache.flink.api.connector.source.SourceReaderContext;
 import org.apache.flink.api.connector.source.SplitEnumerator;
 import org.apache.flink.api.connector.source.SplitEnumeratorContext;
 import org.apache.flink.configuration.Configuration;
-import org.apache.flink.connector.hbase.source.enumerator.HbaseSourceEnumerator;
+import org.apache.flink.connector.hbase.source.enumerator.HbaseSplitEnumerator;
 import org.apache.flink.connector.hbase.source.reader.HbaseSourceReader;
 import org.apache.flink.connector.hbase.source.split.HbaseSourceSplit;
 import org.apache.flink.connector.hbase.source.split.HbaseSourceSplitSerializer;
 import org.apache.flink.core.io.SimpleVersionedSerializer;
 
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.Collection;
 import java.util.List;
 
 /** A connector for Hbase. */
 public class HbaseSource implements Source<byte[], HbaseSourceSplit, Collection<HbaseSourceSplit>> {
 
+    public static org.apache.hadoop.conf.Configuration TEMP_hbaseConf; // TODO remove asap
+
     private final Boundedness boundedness;
 
-    public HbaseSource(Boundedness boundedness) {
+    private final String tableName;
+    private final transient org.apache.hadoop.conf.Configuration
+            hbaseConfiguration; // TODO find out why source needs to be serializable
+
+    public HbaseSource(
+            Boundedness boundedness,
+            String tableName,
+            org.apache.hadoop.conf.Configuration hbaseConfiguration) {
         this.boundedness = boundedness;
+        this.tableName = tableName;
+        this.hbaseConfiguration = hbaseConfiguration;
+
+        TEMP_hbaseConf = hbaseConfiguration;
     }
 
     @Override
@@ -45,7 +59,9 @@ public class HbaseSource implements Source<byte[], HbaseSourceSplit, Collection<
             throws Exception {
         System.out.println("restoreEnumerator");
 
-        return new HbaseSourceEnumerator(enumContext, checkpoint);
+        HbaseSplitEnumerator enumerator = new HbaseSplitEnumerator(enumContext);
+        enumerator.addSplits(checkpoint);
+        return enumerator;
     }
 
     @Override
@@ -53,10 +69,21 @@ public class HbaseSource implements Source<byte[], HbaseSourceSplit, Collection<
             SplitEnumeratorContext<HbaseSourceSplit> enumContext) throws Exception {
         System.out.println("createEnumerator");
         List<HbaseSourceSplit> splits = new ArrayList<>();
-        splits.add(
-                new HbaseSourceSplit(
-                        "1234", "localhost", "test", new org.apache.hadoop.conf.Configuration()));
-        return new HbaseSourceEnumerator(enumContext, splits);
+
+        List<String> regionIds = Arrays.asList("region1", "region2");
+        regionIds.forEach(
+                regionId -> {
+                    splits.add(
+                            new HbaseSourceSplit(
+                                    String.format("1234%s", regionId),
+                                    "localhost",
+                                    tableName,
+                                    regionId,
+                                    hbaseConfiguration));
+                });
+        HbaseSplitEnumerator enumerator = new HbaseSplitEnumerator(enumContext);
+        enumerator.addSplits(splits);
+        return enumerator;
     }
 
     @Override
@@ -70,7 +97,6 @@ public class HbaseSource implements Source<byte[], HbaseSourceSplit, Collection<
     public SimpleVersionedSerializer<Collection<HbaseSourceSplit>>
             getEnumeratorCheckpointSerializer() {
         System.out.println("getEnumeratorCheckpointSerializer");
-
         return null;
     }
 }
