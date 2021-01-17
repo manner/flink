785f3f53ab01736082e18501b8edf0e82de34faa
Moritz Manner <moritz.manner@gmail.com>
2021-01-17 17:56:12 +0100

    Add DemoIngester and DemoSchema

---
+++ flink-connectors/flink-connector-hbase/src/test/java/org/apache/flink/connector/hbase/source/hbasemocking/DemoSchema.java
@@ -0,0 +1,49 @@
+/*
+ * Copyright 2012 NGDATA nv
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package org.apache.flink.connector.hbase.source.hbasemocking;
+
+import org.apache.hadoop.conf.Configuration;
+import org.apache.hadoop.hbase.HColumnDescriptor;
+import org.apache.hadoop.hbase.HTableDescriptor;
+import org.apache.hadoop.hbase.TableName;
+import org.apache.hadoop.hbase.client.Admin;
+import org.apache.hadoop.hbase.client.ConnectionFactory;
+
+import java.io.IOException;
+
+/** Adding DemoSchema to Hbase Test Cluster. */
+public class DemoSchema {
+    public static void main(String[] args) throws Exception {
+        Configuration conf = TestClusterStarter.getConfig();
+        createSchema(conf);
+    }
+
+    public static void createSchema(Configuration hbaseConf) throws IOException {
+        Admin admin = ConnectionFactory.createConnection(hbaseConf).getAdmin();
+        if (!admin.tableExists(TableName.valueOf("sep-user-demo"))) {
+            HTableDescriptor tableDescriptor =
+                    new HTableDescriptor(TableName.valueOf("sep-user-demo"));
+
+            HColumnDescriptor infoCf = new HColumnDescriptor("info");
+            infoCf.setScope(1);
+            tableDescriptor.addFamily(infoCf);
+
+            admin.createTable(tableDescriptor);
+        }
+        admin.close();
+    }
+}
