44640a672b72e862f7507a91392ff668e0f21742
Leon Bein <leon.bein@student.hpi.de>
2021-01-20 15:36:58 +0100

    Rename classes to begin with two capital letters

--- flink-connectors/flink-connector-hbase/src/main/java/org/apache/flink/connector/hbase/source/enumerator/HBaseSplitEnumerator.java
+++ flink-connectors/flink-connector-hbase/src/main/java/org/apache/flink/connector/hbase/source/enumerator/HBaseSplitEnumerator.java
@@ -3,7 +3,7 @@ package org.apache.flink.connector.hbase.source.enumerator;
 import org.apache.flink.annotation.Internal;
 import org.apache.flink.api.connector.source.SplitEnumerator;
 import org.apache.flink.api.connector.source.SplitEnumeratorContext;
-import org.apache.flink.connector.hbase.source.split.HbaseSourceSplit;
+import org.apache.flink.connector.hbase.source.split.HBaseSourceSplit;
 
 import javax.annotation.Nullable;
 
@@ -14,12 +14,12 @@ import java.util.Queue;
 
 /** The enumerator class for Hbase source. */
 @Internal
-public class HbaseSplitEnumerator
-        implements SplitEnumerator<HbaseSourceSplit, Collection<HbaseSourceSplit>> {
-    private final SplitEnumeratorContext<HbaseSourceSplit> context;
-    private final Queue<HbaseSourceSplit> remainingSplits;
+public class HBaseSplitEnumerator
+        implements SplitEnumerator<HBaseSourceSplit, Collection<HBaseSourceSplit>> {
+    private final SplitEnumeratorContext<HBaseSourceSplit> context;
+    private final Queue<HBaseSourceSplit> remainingSplits;
 
-    public HbaseSplitEnumerator(SplitEnumeratorContext<HbaseSourceSplit> context) {
+    public HBaseSplitEnumerator(SplitEnumeratorContext<HBaseSourceSplit> context) {
         this.context = context;
         this.remainingSplits = new ArrayDeque<>();
     }
@@ -34,7 +34,7 @@ public class HbaseSplitEnumerator
 
     @Override
     public void handleSplitRequest(int subtaskId, @Nullable String requesterHostname) {
-        final HbaseSourceSplit nextSplit = remainingSplits.poll();
+        final HBaseSourceSplit nextSplit = remainingSplits.poll();
         if (nextSplit != null) {
             context.assignSplit(nextSplit, subtaskId);
         } else {
@@ -43,14 +43,14 @@ public class HbaseSplitEnumerator
     }
 
     @Override
-    public void addSplitsBack(List<HbaseSourceSplit> splits, int subtaskId) {
+    public void addSplitsBack(List<HBaseSourceSplit> splits, int subtaskId) {
         remainingSplits.addAll(splits);
     }
 
     @Override
     public void addReader(int subtaskId) {
         System.out.println("addReader");
-        HbaseSourceSplit nextSplit = remainingSplits.poll();
+        HBaseSourceSplit nextSplit = remainingSplits.poll();
         if (nextSplit != null) {
             context.assignSplit(nextSplit, subtaskId);
         } else {
@@ -59,11 +59,11 @@ public class HbaseSplitEnumerator
     }
 
     @Override
-    public Collection<HbaseSourceSplit> snapshotState() throws Exception {
+    public Collection<HBaseSourceSplit> snapshotState() throws Exception {
         return remainingSplits;
     }
 
-    public void addSplits(Collection<HbaseSourceSplit> splits) {
+    public void addSplits(Collection<HBaseSourceSplit> splits) {
         remainingSplits.addAll(splits);
     }
 }
