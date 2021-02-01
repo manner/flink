eb6340f3760175e28e6d981b1fba31a94adaa95a
Moritz Manner <moritz.manner@gmail.com>
2021-02-01 16:51:11 +0100

    Finish simple sink test

--- flink-connectors/flink-connector-hbase/src/test/java/org/apache/flink/connector/hbase/source/hbaseendpoint/HBaseSinkTests.java
+++ flink-connectors/flink-connector-hbase/src/test/java/org/apache/flink/connector/hbase/source/hbaseendpoint/HBaseSinkTests.java
@@ -8,7 +8,19 @@ import org.apache.flink.connector.hbase.source.hbasemocking.HBaseTestClusterUtil
 import org.apache.flink.streaming.api.datastream.DataStream;
 import org.apache.flink.streaming.api.environment.StreamExecutionEnvironment;
 
+import org.apache.hadoop.conf.Configuration;
+import org.apache.hadoop.hbase.TableName;
+import org.apache.hadoop.hbase.client.Connection;
+import org.apache.hadoop.hbase.client.ConnectionFactory;
+import org.apache.hadoop.hbase.client.Get;
+import org.apache.hadoop.hbase.client.Result;
+import org.apache.hadoop.hbase.client.Table;
+import org.apache.hadoop.hbase.util.Bytes;
 import org.junit.Test;
+
+import java.io.IOException;
+
+import static org.junit.Assert.assertEquals;
 
 /** Test for {@link org.apache.flink.connector.hbase.sink.HBaseSink}. */
 public class HBaseSinkTests extends TestsWithTestHBaseCluster {
@@ -16,16 +28,32 @@ public class HBaseSinkTests extends TestsWithTestHBaseCluster {
     @Test
     public void testSimpleSink() throws Exception {
         final StreamExecutionEnvironment env = StreamExecutionEnvironment.getExecutionEnvironment();
-        env.setParallelism(1);
+        Configuration hbaseConfiguration = HBaseTestClusterUtil.getConfig();
+
+        String tableName = "test-table";
+        long start = 1;
+        long end = 10;
+
         final DataStream<Long> numberSource =
                 env.fromSource(
-                        new NumberSequenceSource(1L, 10L),
+                        new NumberSequenceSource(start, end),
                         WatermarkStrategy.noWatermarks(),
                         "numberSource");
 
-        final HBaseSink<Long> hbaseSink =
-                new HBaseSink<>("test-table", HBaseTestClusterUtil.getConfig());
+        final HBaseSink<Long> hbaseSink = new HBaseSink<>(tableName, hbaseConfiguration);
         numberSource.sinkTo(hbaseSink);
         env.execute();
+
+        try (Connection connection = ConnectionFactory.createConnection(hbaseConfiguration)) {
+            Table table = connection.getTable(TableName.valueOf(tableName));
+            for (long i = start; i <= end; i++) {
+                Get get = new Get(Bytes.toBytes(String.valueOf(i)));
+                Result r = table.get(get);
+                byte[] value = r.getValue("info".getBytes(), "age".getBytes());
+                assertEquals(Long.parseLong(new String(value)), i);
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
     }
 }
