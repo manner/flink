6dabef3d530df25b56f99344b95807236592c805
Moritz Manner <moritz.manner@gmail.com>
2021-02-01 16:17:12 +0100

    Remove non-essential sink code

--- flink-connectors/flink-connector-hbase/src/main/java/org/apache/flink/connector/hbase/sink/writer/HBaseWriter.java
+++ flink-connectors/flink-connector-hbase/src/main/java/org/apache/flink/connector/hbase/sink/writer/HBaseWriter.java
@@ -4,36 +4,47 @@ import org.apache.flink.api.connector.sink.Sink;
 import org.apache.flink.api.connector.sink.SinkWriter;
 import org.apache.flink.connector.hbase.sink.HBaseSinkCommittable;
 
+import org.apache.hadoop.conf.Configuration;
+import org.apache.hadoop.hbase.TableName;
+import org.apache.hadoop.hbase.client.Connection;
+import org.apache.hadoop.hbase.client.ConnectionFactory;
 import org.apache.hadoop.hbase.client.Put;
+import org.apache.hadoop.hbase.client.Table;
 import org.apache.hadoop.hbase.util.Bytes;
 
 import java.io.IOException;
-import java.util.ArrayList;
 import java.util.Collections;
 import java.util.List;
 
 /** HBaseWriter. */
 public class HBaseWriter<IN> implements SinkWriter<IN, HBaseSinkCommittable, HBaseWriterState> {
 
-    private List<HBaseSinkCommittable> elements;
+    private final Configuration hbaseConfiguration;
+    private final TableName tableName;
 
-    public HBaseWriter(Sink.InitContext context) {
-        elements = new ArrayList<>();
+    public HBaseWriter(
+            Sink.InitContext context, TableName tableName, Configuration hbaseConfiguration) {
+        this.hbaseConfiguration = hbaseConfiguration;
+        this.tableName = tableName;
     }
 
     @Override
     public void write(IN element, Context context) throws IOException {
-        Put put = new Put(Bytes.toBytes("row"));
+        Put put = new Put(Bytes.toBytes(element.toString()));
         put.addColumn(
                 Bytes.toBytes("info"), Bytes.toBytes("age"), Bytes.toBytes(element.toString()));
-        elements.add(new HBaseSinkCommittable(put));
+        try (Connection connection =
+                ConnectionFactory.createConnection(this.hbaseConfiguration); ) {
+            Table table = connection.getTable(tableName);
+            table.put(put);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
     }
 
     @Override
     public List<HBaseSinkCommittable> prepareCommit(boolean flush) throws IOException {
-        List<HBaseSinkCommittable> result = elements;
-        elements = new ArrayList<>();
-        return result;
+        return Collections.emptyList();
     }
 
     @Override
