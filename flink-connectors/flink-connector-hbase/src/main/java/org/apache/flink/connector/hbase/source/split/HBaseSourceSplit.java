071b1d1f1412d3eb2a6e34fa7cb26bf5ec69c71d
Leon Bein <leon.bein@student.hpi.de>
2021-02-11 19:46:50 +0100

    Stamp-based checkpointing
    - Added stamps to HBaseEvent class that are unique and induce a strict temporal order
    - Added last stamp to SplitState
       - Used in RecordEmitter to filter out events that are potentially already seen
       - persistet by storing in serialized split

--- flink-connectors/flink-connector-hbase/src/main/java/org/apache/flink/connector/hbase/source/split/HBaseSourceSplit.java
+++ flink-connectors/flink-connector-hbase/src/main/java/org/apache/flink/connector/hbase/source/split/HBaseSourceSplit.java
@@ -1,6 +1,7 @@
 package org.apache.flink.connector.hbase.source.split;
 
 import org.apache.flink.api.connector.source.SourceSplit;
+import org.apache.flink.api.java.tuple.Tuple2;
 
 import org.apache.hadoop.conf.Configuration;
 
@@ -21,13 +22,26 @@ public class HBaseSourceSplit implements SourceSplit, Serializable {
 
     private final Configuration hbaseConf; // TODO serialization
 
+    private final Tuple2<Long, Integer> firstEventStamp;
+
     public HBaseSourceSplit(
             String id, String host, String table, String columnFamily, Configuration hbaseConf) {
+        this(id, host, table, columnFamily, hbaseConf, Tuple2.of(-1L, -1));
+    }
+
+    public HBaseSourceSplit(
+            String id,
+            String host,
+            String table,
+            String columnFamily,
+            Configuration hbaseConf,
+            Tuple2<Long, Integer> firstEventStamp) {
         this.id = id;
         this.host = host;
         this.table = table;
         this.columnFamily = columnFamily;
         this.hbaseConf = hbaseConf;
+        this.firstEventStamp = firstEventStamp;
     }
 
     public String getTable() {
@@ -54,5 +68,14 @@ public class HBaseSourceSplit implements SourceSplit, Serializable {
     @Override
     public String toString() {
         return String.format("HbaseSourceSplit: %s %s", getHost(), getTable());
+    }
+
+    public Tuple2<Long, Integer> getFirstEventStamp() {
+        return firstEventStamp;
+    }
+
+    public HBaseSourceSplit withStamp(long lastTimeStamp, int lastIndex) {
+        return new HBaseSourceSplit(
+                id, host, table, columnFamily, hbaseConf, Tuple2.of(lastTimeStamp, lastIndex));
     }
 }
