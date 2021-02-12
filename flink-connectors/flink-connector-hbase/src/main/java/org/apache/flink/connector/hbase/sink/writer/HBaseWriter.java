64060c2074861d8ebe85fd497778ff51cfe7509b
Moritz Manner <moritz.manner@gmail.com>
2021-02-12 18:42:11 +0100

    Use HBaseSinkSerializer for HBaseSink

--- flink-connectors/flink-connector-hbase/src/main/java/org/apache/flink/connector/hbase/sink/writer/HBaseWriter.java
+++ flink-connectors/flink-connector-hbase/src/main/java/org/apache/flink/connector/hbase/sink/writer/HBaseWriter.java
@@ -3,6 +3,7 @@ package org.apache.flink.connector.hbase.sink.writer;
 import org.apache.flink.api.connector.sink.Sink;
 import org.apache.flink.api.connector.sink.SinkWriter;
 import org.apache.flink.connector.hbase.sink.HBaseSinkCommittable;
+import org.apache.flink.connector.hbase.sink.HBaseSinkSerializer;
 
 import org.apache.hadoop.conf.Configuration;
 import org.apache.hadoop.hbase.TableName;
@@ -10,7 +11,6 @@ import org.apache.hadoop.hbase.client.Connection;
 import org.apache.hadoop.hbase.client.ConnectionFactory;
 import org.apache.hadoop.hbase.client.Put;
 import org.apache.hadoop.hbase.client.Table;
-import org.apache.hadoop.hbase.util.Bytes;
 
 import java.io.IOException;
 import java.util.Collections;
@@ -20,32 +20,28 @@ import java.util.List;
 public class HBaseWriter<IN> implements SinkWriter<IN, HBaseSinkCommittable, HBaseWriterState> {
 
     private final Configuration hbaseConfiguration;
-    private final String table;
-    private final String columnFamily;
-    private final String qualifier;
+    private final HBaseSinkSerializer<IN> sinkSerializer;
+    private final String tableName;
 
     public HBaseWriter(
             Sink.InitContext context,
-            String table,
-            String columnFamily,
-            String qualifier,
+            String tableName,
+            HBaseSinkSerializer<IN> sinkSerializer,
             Configuration hbaseConfiguration) {
-        this.table = table;
-        this.columnFamily = columnFamily;
-        this.qualifier = qualifier;
-
+        this.tableName = tableName;
+        this.sinkSerializer = sinkSerializer;
         this.hbaseConfiguration = hbaseConfiguration;
     }
 
     @Override
     public void write(IN element, Context context) throws IOException {
-        Put put = new Put(Bytes.toBytes(element.toString()));
+        Put put = new Put(sinkSerializer.serializeRowKey(element));
         put.addColumn(
-                Bytes.toBytes(columnFamily),
-                Bytes.toBytes(qualifier),
-                Bytes.toBytes(element.toString()));
+                sinkSerializer.serializeColumnFamily(element),
+                sinkSerializer.serializeQualifier(element),
+                sinkSerializer.serializePayload(element));
         try (Connection connection = ConnectionFactory.createConnection(hbaseConfiguration)) {
-            Table table = connection.getTable(TableName.valueOf(this.table));
+            Table table = connection.getTable(TableName.valueOf(tableName));
             table.put(put);
         } catch (IOException e) {
             e.printStackTrace();
