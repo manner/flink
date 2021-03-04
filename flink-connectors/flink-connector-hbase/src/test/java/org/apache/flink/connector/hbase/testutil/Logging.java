b53d1fc9de6d9480331e41b712175e120f08a58c
Leon Bein <leon.bein@student.hpi.de>
2021-03-04 12:42:42 +0100

    Limit logging only to test classes

--- flink-connectors/flink-connector-hbase/src/test/java/org/apache/flink/connector/hbase/testutil/Logging.java
+++ flink-connectors/flink-connector-hbase/src/test/java/org/apache/flink/connector/hbase/testutil/Logging.java
@@ -1,10 +1,7 @@
 package org.apache.flink.connector.hbase.testutil;
 
 import org.apache.logging.log4j.Level;
-import org.apache.logging.log4j.LogManager;
-import org.apache.logging.log4j.core.LoggerContext;
-import org.apache.logging.log4j.core.config.Configuration;
-import org.apache.logging.log4j.core.config.LoggerConfig;
+import org.apache.logging.log4j.core.config.Configurator;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
@@ -19,10 +16,6 @@ public class Logging {
     }
 
     private static void enableLogging() {
-        LoggerContext ctx = (LoggerContext) LogManager.getContext(false);
-        Configuration config = ctx.getConfiguration();
-        LoggerConfig loggerConfig = config.getLoggerConfig(LOGGER_NAME);
-        loggerConfig.setLevel(Level.ALL);
-        ctx.updateLoggers();
+        Configurator.setLevel(LOGGER_NAME, Level.ALL);
     }
 }
