b9ca9c94cabea212b8155ebcaac97f5f80b5f490
Leon Bein <leon.bein@student.hpi.de>
2021-03-09 10:34:33 +0100

    Move ClusterStartTest to playground

--- flink-connectors/flink-connector-hbase/src/test/java/org/apache/flink/connector/hbase/source/playground/ClusterStartTest.java
+++ flink-connectors/flink-connector-hbase/src/test/java/org/apache/flink/connector/hbase/source/playground/ClusterStartTest.java
@@ -16,7 +16,7 @@
  * limitations under the License.
  */
 
-package org.apache.flink.connector.hbase.source;
+package org.apache.flink.connector.hbase.source.playground;
 
 import org.apache.flink.connector.hbase.testutil.HBaseTestClusterUtil;
 
